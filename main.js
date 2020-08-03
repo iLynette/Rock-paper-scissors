dad6ffb8866cab087bdecea02d884a6f6354f3c0
Lynette <lynetteacholah@icloud.com>
2020-08-03 20:38:18 +0300

    More changes

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 const selectionButtons = document.querySelectorAll('[data-selection]')
 const SELECTIONS = [
     {
-        name:'rock'
-        emoji: '👊'
-        beats: 'scissors'
-    }
+        name: 'rock',
+        emoji: '👊',
+        beats: 'paper'
+    },
     {
-        name:'paper'
-        emoji: '🤚'
+        name: 'paper',
+        emoji: '🤚',
         beats: 'scissors'
-    }
+    },
     {
-        name:'scissors'
-        emoji: '✌'
+        name: 'scissors',
+        emoji: '✌',
         beats: 'paper'
     }
 ]
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection
+        const selection = SELECTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selectionName)
+    })
+})
+function makeSelection(selection){
+    console.log(selection)
+}
