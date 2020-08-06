f4cf771c9557dcba59071d1675f59e0c608abbf4
Lynette <lynetteacholah@icloud.com>
2020-08-06 13:41:52 +0300

    hanges

--- main.js
+++ main.js
@@ -1,56 +1,62 @@
-const gameSelection =['rock', 'paper', 'scissors']
+const message = document.querySelector('.message');
+const score = document.querySelector('.score');
+const buttons = document.querySelectorAll('button');
 
-function computerPlay() {
-    let compChoice = gameSelection[Math.floor(Math.random()* gameSelection.length)]
-    return compChoice
-}
-function userPlay() {
-    let input = prompt('rock, paper, scissors')
-    let userSelection= input.trim().toLowerCase()
-    return input
-}
 
-function makeSelection(playerSelection, computerSelection) {
-    if (
-        (playerSelection == "rock" && computerSelection === "scissors") ||
-        (playerSelection == "paper" && computerSelection === "rock") ||
-        (playerSelection == "scissors" && computerSelection === "paper")
-    ) {
-        return 'Win'
-    } else if (
-        (playerSelection == "rock" && computerSelection === "paper") ||
-        (playerSelection == "paper" && computerSelection === "scissors") ||
-        (playerSelection == "scissors" && computerSelection === "scissors")
-    ){
-        return 'Lose'
-    } else {
-        return 'Tie'
+let winner = [0, 0];
+
+for (let i = 0;i < buttons.length; i++){
+    buttons[i].addEventListener('click', playGame);
+}
+function playGame(e) {
+    //console.log(e.target.innerText);
+    let playerSelection =e.target.innerText
+    let computerSelection = Math.random();
+    //console.log(computerSelection);
+    if (computerSelection < 0.34) {
+        computerSelection = 'Rock';
+    }else if (computerSelection <= 0.67) {
+        computerSelection = 'Paper';
+    }else {
+        computerSelection = 'Scissors';
     }
+    console.log(playerSelection, computerSelection);
+    let result = checkWinner(playerSelection, computerSelection);
+    console.log(result);
+    if (result == 'Player') {
+        result += 'Wins!';
+        winner [0]++;
+    } else if (result == 'Computer'){
+        result += 'Wins';
+        winner [1]++;
+    }else {
+        result += 'results in a tie'
+    }
+    score.innerHTML = 'Player['+winner[0] +'] Computer['+winner [1]+']';
 }
-function game() {
-    let playerScore = 0
-    let computerScore = 0
-
-    var i;
-    for (let i = 0; i < 4; i++) {
-        let playerSelection = userPlay()
-        let computerSelection = computerPlay()
-        console.log(`You selected: ${playerSelection}`)
-        console.log(`Computer selected: ${computerSelection}`)
-        let resultOutput = makeSelection(playerSelection, computerSelection)
-
-        if (resultOutput.includes("win", 3)) {
-            playScore++
-          } else {
-            computerScore++
+function checkWinner(player, computer){
+    if (player === computer){
+        return 'Draw';
+    }
+    if (player === 'Rock') {
+        if (computer === 'Paper') {
+            return 'Computer';
+        }else {
+            return 'player';
         }
     }
-    if (playerScore < computerScore) {
-        console.log("Oops !!! You lost!!")
-      } else if (playerScore > computerScore) {
-        console.log("Hurray !!! You won!!")
-      } else {
-        console.log("Its a tie")
-      }
+    if (player === 'Paper'){
+        if (computer === 'Scissors'){
+            return 'Computer';
+        }else {
+            return 'Player'
+        }  
     }
-    game()
+    if (player === 'Scissors') {
+        if (computer === 'Rock') {
+            return 'Player';
+        }else {
+            return 'Computer'
+        }
+    }     
+}
